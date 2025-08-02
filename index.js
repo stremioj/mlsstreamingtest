6db1d78073d7f908e9f488d18aa428521545af5e
stremioj <j4ckchalmers@gmail.com>
2025-08-02 05:28:32 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+module.exports = (req, res) => {
+    const url = new URL(req.url, `https://${req.headers.host}`);
+    const pathname = url.pathname;
+
+    if (pathname === "/api/manifest.json") {
+        const manifest = {
+            id: "ppv.stream.addon",
+            version: "1.0.0",
+            name: "MLS Stream Testing Add-on",
+            description: "Direct m3u8 stream",
+            resources: ["stream"],
+            types: ["movie"],
+            catalogs: []
+        };
+        res.setHeader("Content-Type", "application/json");
+        return res.end(JSON.stringify(manifest));
+    }
+
+    if (pathname.startsWith("/api/stream/")) {
+        const stream = {
+            streams: [
+                {
+                    title: "Live PPV Stream",
+                    url: "https://5nhp186eg31fofnc.chinese-restaurant-api.site/v3/variant/VE1AO1NTbu8mbv12LxEWM21ycrNWYyR3LhhTZ1UzMmlzN3gTMtEGOihTL3EGN00yY4UWYtIjY5YjN2YTZ/master.m3u8",
+                    behaviorHints: {
+                        proxyHeaders: {
+                            request: {
+                                "Referer": "https://ppv.to",
+                                "User-Agent": "Mozilla/5.0"
+                            }
+                        }
+                    }
+                }
+            ]
+        };
+        res.setHeader("Content-Type", "application/json");
+        return res.end(JSON.stringify(stream));
+    }
+
+    res.statusCode = 404;
+    res.end("Not Found");
+};
